12a23eb0cac4ad932f05dae1b02536faa4c0ba8f
Sid <sidkhamithkar@gmail.com>
2021-05-04 00:54:36 +0530

    initial commit

---
+++ napkoin.py
@@ -0,0 +1,69 @@
+blockchain = []
+
+
+def get_last_blockchain_value():
+    if len(blockchain) < 1:
+        return None
+    
+    return blockchain[-1]
+
+
+def add_transaction(transaction_amount, last_transaction = [1]):
+    if last_transaction == None:
+        last_transaction = [1]
+    blockchain.append([last_transaction, transaction_amount])
+
+
+def get_transaction_value():
+    user_input = float(input('Your transaction amount: '))
+    return user_input
+
+
+def get_user_choice():
+    user_input = raw_input('Your choice: ')
+    return user_input
+
+
+def print_blockchain_elements():
+    for block in blockchain:
+      print('Outputting Block')
+      print(block)
+
+
+def verify_chain():
+    block_index = 0
+    is_valid = True
+    for block in blockchain:
+        if block_index == 0:
+            block_index += 1
+            continue
+        elif block[0] == blockchain[block_index - 1]:
+            is_valid = True
+        else:
+            is_valid = False
+            break
+        block_index += 1
+    return is_valid
+
+
+while True:
+    print('Please choose')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    print('q: Quit')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_transaction(tx_amount, get_last_blockchain_value())
+    elif user_choice == '2':
+        print_blockchain_elements()
+    elif user_choice == 'q':
+        break
+    else:
+        print('Error: Input was invalid. \nPlease pick a value from the list.')
+    if not verify_chain():
+        print('Invalid blockchain!')
+        break
+    print('Choice registered!')
+
+print('Done!')
